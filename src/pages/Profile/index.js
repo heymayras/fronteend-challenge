42697d7fb2537d0d7d028cf4c5242e05f4307173
Mayra Gonçalves <mccgoncalves11@gmail.com>
2023-04-11 21:43:43 -0300

    Exibe repositórios do usuário

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import styles from "./Profile.module.css";
-import { BsStar, BsPeople, BsHeart } from "react-icons/bs";
+import { BsStar, BsPeople, BsHeart, BsDot } from "react-icons/bs";
 import axios from "axios";
 import { useParams } from "react-router-dom";
-import { async } from "q";
 
 export function Profile() {
   const { user } = useParams();
@@ -46,34 +45,50 @@ export function Profile() {
     <>
       <div className={styles.sidebar_container}>
         <div className={styles.sidebar}>
-          <div className={styles.profile_image}>
-            <img src={listUser.avatar_url} />
-          </div>
+          <img src={listUser.avatar_url} />
+
           <div className={styles.details_user}>
             <div>
-              <p>Developer's Name</p>
-              <p>@username</p>
-            </div>
-            <div>
-              <p>{listUser.bio}</p>
-            </div>
-            <div className={styles.followers_details}>
-              <BsPeople />
-              <p>{listUser.followers}</p>
-              <p>followers</p>
-            </div>
-            <div className={styles.followers_details}>
-              <BsHeart />
-              <p>{listUser.following}</p>
-              <p>following</p>
-            </div>
-            <div className={styles.followers_details}>
-              <BsStar />
-              <p>100</p>
-              <p>stars</p>
+              <h3>{listUser.name}</h3>
+              <p>@{listUser.login}</p>
             </div>
           </div>
+          <div className={styles.biografia}>
+            <p>{listUser.bio}</p>
+          </div>
+          <div className={styles.followers_details}>
+            <BsPeople />
+            <p>{listUser.followers}</p>
+            <p>followers</p>
+            <BsHeart />
+            <p>{listUser.following}</p>
+            <p>following</p>
+            <BsStar />
+            <p>0</p>
+            <p>stars</p>
+          </div>
         </div>
+      </div>
+      <div className={styles.listaRepos}>
+        {repos &&
+          repos.map((repo) => {
+            console.log(repo);
+
+            return (
+              <>
+                <div className={styles.dadosRepos}>
+                  <a href={repo.html_url}>
+                    <h2>{repo.full_name}</h2>
+                  </a>
+                  <h3> {repo.description}</h3>
+                  <p>
+                    <BsStar /> {repo.stargazers_count} <BsDot />{" "}
+                    {repo.updated_at}
+                  </p>
+                </div>
+              </>
+            );
+          })}
       </div>
     </>
   );
